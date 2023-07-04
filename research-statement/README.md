c477e4878fb9b8343a0c3eafa469afd2fe650b89
Stanislaw Baranski <stan@stan.bar>
2023-07-04 17:30:17 +0200

    remove reserach statement

--- research-statement/README.md
+++
@@ -1,4 +0,0 @@
-# Personality type prediction based on digital activity using machine learning
-
-1. Run `make all`
-
--- research-statement/pull.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp /Users/stanbar/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents/Slip-box/references/bibliography.bib .
-cp -r /Users/stanbar/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents/Slip-box/MBTI-Prediction/Research-statement-polish.md polish.md
